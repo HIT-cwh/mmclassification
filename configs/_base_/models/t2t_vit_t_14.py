5b1e737dd1dfa206fc541a860e0afe78e2afc676
HIT-cwh <2892770585@qq.com>
2021-05-16 14:04:35 +0800

    add batch_first

--- configs/_base_/models/t2t_vit_t_14.py
+++ configs/_base_/models/t2t_vit_t_14.py
@@ -29,7 +29,8 @@ model = dict(
                     num_fcs=2,
                     act_cfg=dict(type='GELU'),
                     dropout_layer=dict(type='DropPath')),
-                operation_order=('norm', 'self_attn', 'norm', 'ffn')),
+                operation_order=('norm', 'self_attn', 'norm', 'ffn'),
+                batch_first=True),
             drop_path_rate=0.1)),
     neck=None,
     head=dict(
