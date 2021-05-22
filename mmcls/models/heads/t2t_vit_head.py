3070b9eab5bb16cba83533d77e1c6a46e1a65ef1
HIT-cwh <2892770585@qq.com>
2021-05-22 22:32:27 +0800

    replace mm.Module with BaseModule in heads

--- mmcls/models/heads/t2t_vit_head.py
+++ mmcls/models/heads/t2t_vit_head.py
@@ -8,10 +8,11 @@ from .linear_head import LinearClsHead
 @HEADS.register_module()
 class T2THead(LinearClsHead):
 
-    def __init__(self, *args, **kwargs):
-        super(T2THead, self).__init__(*args, **kwargs)
+    def __init__(self, init_cfg=None, *args, **kwargs):
+        super(T2THead, self).__init__(init_cfg=init_cfg, *args, **kwargs)
 
     def init_weights(self):
+        super(T2THead, self).init_weights()
         trunc_normal_(self.fc.weight, std=.02)
         if isinstance(self.fc, nn.Linear) and self.fc.bias is not None:
             nn.init.constant_(self.fc.bias, 0)
