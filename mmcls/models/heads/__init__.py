0164c490e8aaf67dd486b8fb870072894a8ae6a6
HIT-cwh <2892770585@qq.com>
2021-05-22 12:52:13 +0800

    add T2THead

--- mmcls/models/heads/__init__.py
+++ mmcls/models/heads/__init__.py
@@ -1,7 +1,8 @@
 from .cls_head import ClsHead
-from .linear_head import LinearClsHead, T2THead
+from .linear_head import LinearClsHead
 from .multi_label_head import MultiLabelClsHead
 from .multi_label_linear_head import MultiLabelLinearClsHead
+from .t2t_vit_head import T2THead
 from .vision_transformer_head import VisionTransformerClsHead
 
 __all__ = [
