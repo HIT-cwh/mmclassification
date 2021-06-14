0f2abd63250d89c50c869879df4de6518286d185
HIT-cwh <2892770585@qq.com>
2021-06-14 22:46:41 +0800

    LabelSmoothLoss mode=original

--- configs/t2t_vit/t2t_vit_t_14_train_from_origin.py
+++ configs/t2t_vit/t2t_vit_t_14_train_from_origin.py
@@ -36,7 +36,8 @@ model = dict(
         type='LinearClsHead',
         num_classes=1000,
         in_channels=384,
-        loss=dict(type='LabelSmoothLoss', label_smooth_val=0.1),
+        loss=dict(
+            type='LabelSmoothLoss', label_smooth_val=0.1, mode='original'),
         topk=(1, 5)),
     init_cfg=[
         dict(type='TruncNormal', layer='Linear', std=.02),
