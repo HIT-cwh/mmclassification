07b8393311aed663b6963495a6b30c3a51f9e0cd
HIT-cwh <2892770585@qq.com>
2021-06-14 15:31:55 +0800

    delete T2T_Head

--- mmcls/models/heads/__init__.py
+++ mmcls/models/heads/__init__.py
@@ -2,10 +2,9 @@ from .cls_head import ClsHead
 from .linear_head import LinearClsHead
 from .multi_label_head import MultiLabelClsHead
 from .multi_label_linear_head import MultiLabelLinearClsHead
-from .t2t_vit_head import T2THead
 from .vision_transformer_head import VisionTransformerClsHead
 
 __all__ = [
     'ClsHead', 'LinearClsHead', 'MultiLabelClsHead', 'MultiLabelLinearClsHead',
-    'VisionTransformerClsHead', 'T2THead'
+    'VisionTransformerClsHead'
 ]
